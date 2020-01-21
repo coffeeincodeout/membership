9b790644f3419e513296e40ff76a96975cc1e520
coffeeincodeout <david@colossalpoint.com>
2020-01-20 22:14:43 -0500

    edited serializer

--- useraccounts/serializers.py
+++ useraccounts/serializers.py
@@ -8,13 +8,3 @@ class MembersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Members
         fields = ['pk', 'data']
-
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['url', 'username', 'email', 'groups']
-#
-# class GroupSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Group
-#         fields = ['url', 'name']
